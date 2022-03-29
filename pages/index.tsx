af5794683d1afd5eccdadfd429024a43c47792b4
Honza Vrkota <vrkota.jan@hotmail.com>
2022-03-29 15:07:19 +0200

    var not used

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import styles from "../styles/Home.module.css";
  */
 const Home: NextPage = () => {
   // const test = `honza`;
-  // const honza = `test`;
+  const honza = `test`;
 
   return (
     <div className={styles.container}>
