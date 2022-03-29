cba964b1c5c95866e0091857ad3bf95959b25c5e
Honza Vrkota <vrkota.jan@hotmail.com>
2022-03-29 15:16:51 +0200

    add about text

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,6 +3,31 @@ const About = () => {
     <div>
       Enter
       <p>Test</p>
+      <div>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique
+          recusandae aperiam dolores, laudantium laboriosam veritatis totam
+          neque illum amet debitis sint, dicta veniam. Tenetur, velit aut!
+          Voluptas ut quos aperiam.
+        </p>
+        <p>
+          Quasi vel esse numquam aliquam possimus officia accusantium
+          perferendis at impedit sunt temporibus deserunt minima sit accusamus
+          perspiciatis itaque laborum rem consequatur in, labore eaque corrupti
+          odio! Aspernatur, consectetur quas?
+        </p>
+        <p>
+          Pariatur voluptatibus optio sint, culpa distinctio quis odio ipsa nemo
+          quasi neque accusantium iure, quam cupiditate magnam expedita enim
+          autem, id hic omnis. Velit sequi deleniti aliquam officia nostrum
+          sint.
+        </p>
+        <p>
+          Non molestiae vel repellat vero dolorem illo tempore, culpa quas
+          accusamus mollitia blanditiis hic ex cum labore minus expedita dicta
+          perferendis sunt atque. Ab, vitae minima impedit eaque laborum rerum?
+        </p>
+      </div>
     </div>
   );
 };
